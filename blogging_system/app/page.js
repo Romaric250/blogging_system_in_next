b0ca440b2dcb2f65f9b7d4394f40a4d6cb3a135c
Romaric250 <lonfonyuyromeo@gmail.com>
2024-03-18 23:04:36 +0100

    navigation bar and footer setup successfully;

--- blogging_system/app/page.js
+++ blogging_system/app/page.js
@@ -1,10 +1,23 @@
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      lorem200
+    <div>
       
-    </main>
+      this is a very good thing for us to do and also to imporve our knowleged and technical knwohow
+
+      <div>
+
+        how are you doing this is crazy cause things are going tough
+        how are you doing this is crazy cause things are going tough
+        how are you doing this is crazy cause things are going tough
+        how are you doing this is crazy cause things are going tough
+        how are you doing this is crazy cause things are going tough
+        how are you doing this is crazy cause things are going tough
+      </div>
+      
+    </div>
+
   );
 }
---
+++ blogging_system/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react';
+import Link from 'next/link';
+import { useState } from 'react';
+import styles from './navbar.module.css';
+
+const Navbar = () => {
+    return (
+        <div className= {styles.container}>
+Navigation
+
+        </div>
+    )
+}
+
+export default Navbar
