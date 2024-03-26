b1437152e1ee5c8da9479564272e910403841933
Romaric250 <lonfonyuyromeo@gmail.com>
2024-03-26 11:29:08 +0100

    feat: post and add comment functinionality completed

--- blogging_system/components/Menu/Menu.jsx
+++ blogging_system/components/Menu/Menu.jsx
@@ -1,89 +1,24 @@
-import React from 'react'
-import styles from "./menu.module.css"
-import Link from "next/link"
+import React from "react";
+import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import MenuPosts from "../../components/menuPosts/MenuPosts";
+import MenuCategories from "../../components/menuCategories/MenuCategories";
 
 const Menu = () => {
   return (
-      <div className={styles.container}>
-          <h2 className={styles.subtitle}>{"whats is hot"}</h2>
-          
-          <h1 className={styles.title}>Most Popular</h1>
-
-          <div className={styles.items}>
-              <Link href="/" className={styles.item}>
-                  <div className={styles.image}>
-                      
-                  </div>
-                  <div className={styles.textContainer}>
-                      <span className={`${styles.category} ${styles.travel}`}>Travel</span>
-                      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, aspernatur?</h3>   
-
-                      <div className={styles.detail}>
-                          <span className={styles.username}> Romaric Lonfonyuy</span>
-                          <span className={styles.date}>20.03.2024</span>
-                      </div>                      
-                  </div>
-              
-                  
-              
-              </Link>
-              <Link href="/" className={styles.item}>
-                  <div className={styles.image}>
-                      
-                  </div>
-                  <div className={styles.textContainer}>
-                      <span className={`${styles.category} ${styles.travel}`}>Travel</span>
-                      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, aspernatur?</h3>   
-
-                      <div className={styles.detail}>
-                          <span className={styles.username}> Romaric Lonfonyuy</span>
-                          <span className={styles.date}>20.03.2024</span>
-                      </div>                      
-                  </div>
-              
-                  
-              
-              </Link>
-              <Link href="/" className={styles.item}>
-                  <div className={styles.image}>
-                     
-                  </div>
-                  <div className={styles.textContainer}>
-                      <span className={`${styles.category} ${styles.travel}`}>Travel</span>
-                      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, aspernatur?</h3>   
-
-                      <div className={styles.detail}>
-                          <span className={styles.username}> Romaric Lonfonyuy</span>
-                          <span className={styles.date}>20.03.2024</span>
-                      </div>                      
-                  </div>
-              
-                  
-              
-              </Link>
-              <Link href="/" className={styles.item}>
-                  <div className={styles.image}>
-                      
-                  </div>
-                  <div className={styles.textContainer}>
-                      <span className={`${styles.category} ${styles.travel}`}>Travel</span>
-                      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, aspernatur?</h3>   
-
-                      <div className={styles.detail}>
-                          <span className={styles.username}> Romaric Lonfonyuy</span>
-                          <span className={styles.date}>20.03.2024</span>
-                      </div>                      
-                  </div>
-              
-                  
-              
-              </Link>
-              
-              
-          </div>
-
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <MenuPosts withImage={false} />
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage={true} />
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
